365f8ea1d50b2f18aab569496c6ac78490eb7139
shahriar <shhrrtnvr@gmail.com>
2024-01-23 09:16:00 +0600

    update script

--- src/main/java/com/shhrrtnvr/smartaquaculture/service/PredictionService.java
+++ src/main/java/com/shhrrtnvr/smartaquaculture/service/PredictionService.java
@@ -6,21 +6,40 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PredictionService {
   private final ResourceLoader resourceLoader;
+
   public Double callPythonMethod(double ph, double watertemp_oc, double airtemp_oc,
                                         double solarradiation, double solarenergy,
                                         double uvindex, double humid_rh) {
     try {
-      Resource resource = resourceLoader.getResource("classpath:script/do_predictor.py");
-      String pythonScriptPath = resource.getFile().getAbsolutePath();
+      Resource scriptResource = resourceLoader.getResource("classpath:script/do_predictor.py");
+      Resource modelResource = resourceLoader.getResource("classpath:script/model.pkl");
+
+      // Create a temporary directory
+      Path tempDir = Files.createTempDirectory("prediction");
+
+      // Create temporary files for the script and model
+      Path tempScript = tempDir.resolve("do_predictor.py");
+      Path tempModel = tempDir.resolve("model.pkl");
+
+      // Copy the script and model to the temporary files
+      try (InputStream in = scriptResource.getInputStream()) {
+        Files.copy(in, tempScript, StandardCopyOption.REPLACE_EXISTING);
+      }
+      try (InputStream in = modelResource.getInputStream()) {
+        Files.copy(in, tempModel, StandardCopyOption.REPLACE_EXISTING);
+      }
+
+      String pythonScriptPath = tempScript.toAbsolutePath().toString();
 
       ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonScriptPath,
           String.valueOf(ph), String.valueOf(watertemp_oc), String.valueOf(airtemp_oc),
