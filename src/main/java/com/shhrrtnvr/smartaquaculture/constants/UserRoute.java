40b0a88a564f2d43fbaffa2d5f9a99b4e6c27d57
shahriar <shhrrtnvr@gmail.com>
2024-01-30 18:52:03 +0600

    admin route for user info

--- src/main/java/com/shhrrtnvr/smartaquaculture/constants/UserRoute.java
+++ src/main/java/com/shhrrtnvr/smartaquaculture/constants/UserRoute.java
@@ -3,4 +3,5 @@ package com.shhrrtnvr.smartaquaculture.constants;
 public interface UserRoute {
   String INFO = "/info";
   String LIST = "/list";
+  String INFO_ADMIN = "/info/{id}";
 }
--- src/main/java/com/shhrrtnvr/smartaquaculture/controller/UserController.java
+++ src/main/java/com/shhrrtnvr/smartaquaculture/controller/UserController.java
@@ -9,12 +9,10 @@ import com.shhrrtnvr.smartaquaculture.factory.mapper.UserMapper;
 import com.shhrrtnvr.smartaquaculture.io.UserResponse;
 import com.shhrrtnvr.smartaquaculture.model.User;
 import com.shhrrtnvr.smartaquaculture.repository.UserRepository;
+import jakarta.security.auth.message.AuthException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +40,19 @@ public class UserController {
   @GetMapping(UserRoute.INFO)
   public ResponseEntity<UserResponse> getInfo(@RequestingUser JwtClaim claim) {
     var user = userRepository.findById(claim.getUserId());
+    return ResponseEntity.ok(
+        UserMapper.toUserResponse(user.orElse(new User()))
+    );
+  }
+
+  @GetMapping(UserRoute.INFO_ADMIN)
+  public ResponseEntity<UserResponse> getInfoByAdmin(
+      @PathVariable Long id,
+      @RequestingUser JwtClaim claim
+  ) throws AuthException {
+    if (claim.getRole() != Role.ADMIN) throw new AuthException("Not Allowed");
+
+    var user = userRepository.findById(id);
     return ResponseEntity.ok(
         UserMapper.toUserResponse(user.orElse(new User()))
     );
