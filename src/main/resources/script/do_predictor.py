40d3391ba4e78979383e26a4dffb5b84a7caec15
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-02-01 03:06:28 +0600

    Model up

--- src/main/resources/script/do_predictor.py
+++ src/main/resources/script/do_predictor.py
@@ -2,6 +2,13 @@ import os
 import pickle
 import sys
 
+class EnsembleModel:
+    def predict(self, X_test):
+            X_test = self.scaler_X.transform(X_test)
+            y_pred = self.ensemble.predict(X_test)
+            y_pred = self.scaler_Y.inverse_transform(y_pred.reshape(-1, 1))
+            return y_pred
+
 def predict_do(ph, watertemp_oc, airtemp_oc, solarradiation, solarenergy, uvindex, humid_rh):
     # Function implementation here
     pass
@@ -10,7 +17,7 @@ def predict_do(ph, watertemp_oc, airtemp_oc, solarradiation, solarenergy, uvinde
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Construct the path to the model file
-model_path = os.path.join(script_dir, 'model.pkl')
+model_path = os.path.join(script_dir, 'ensemble_model.pkl')
 
 # Load the model
 with open(model_path, 'rb') as file:
@@ -18,8 +25,8 @@ with open(model_path, 'rb') as file:
 
 # Extract parameters from command-line arguments
 ph, watertemp_oc, airtemp_oc, solarradiation, solarenergy, uvindex, humid_rh = map(float, sys.argv[1:])
-features = [[ph, watertemp_oc, airtemp_oc, solarradiation, solarenergy, uvindex, humid_rh]]
+features = [[ph, watertemp_oc, airtemp_oc, solarradiation, humid_rh]]
 
 # Example usage
 result = model.predict(features)
-print(result[0])
+print(result[0][0])
