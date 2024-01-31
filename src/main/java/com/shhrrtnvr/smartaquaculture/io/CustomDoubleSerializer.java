32b946645b1283584dfcb07953818abaa65eec2a
shahriar <shhrrtnvr@gmail.com>
2024-02-01 05:59:41 +0600

    Updated to 2 dec

--- src/main/java/com/shhrrtnvr/smartaquaculture/io/CustomDoubleSerializer.java
+++ src/main/java/com/shhrrtnvr/smartaquaculture/io/CustomDoubleSerializer.java
@@ -5,15 +5,17 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CustomDoubleSerializer extends JsonSerializer<Double> {
-    private static final DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     public void serialize(Double value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         if (value != null) {
-            gen.writeString(df.format(value));
+            BigDecimal bd = new BigDecimal(Double.toString(value));
+            bd = bd.setScale(2, RoundingMode.HALF_UP);
+            gen.writeNumber(bd.doubleValue());
         }
     }
 }
