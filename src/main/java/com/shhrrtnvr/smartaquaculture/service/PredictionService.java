5552560edca4612bd25e51b5ee508254419f7185
shahriar <shhrrtnvr@gmail.com>
2024-02-12 11:35:12 +0600

    limit

--- src/main/java/com/shhrrtnvr/smartaquaculture/service/PredictionService.java
+++ src/main/java/com/shhrrtnvr/smartaquaculture/service/PredictionService.java
@@ -59,7 +59,7 @@ public class PredictionService {
       if (exitCode == 0) {
         var d = Double.parseDouble(result.toString().trim());
         if (d > 9) {
-          return d * 0.17078801;
+          d = d * 0.17078801;
         }
         if (d < 4.5) return Math.max(d, 4.5);
         else return Math.min(d, 11.5);
